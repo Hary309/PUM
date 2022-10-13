494874197377029b4e6c5db941773138a533d9b8
Piotr Krupa <piotrkrupa06@gmail.com>
2022-10-13 16:41:00 +0200

    Add first animation

--- lab1b/script.js
+++ lab1b/script.js
@@ -1,13 +1,21 @@
 const WIDTH = 800;
 const HEIGHT = 600;
+const COLOR_GREEN = "#00FF00";
+const COLOR_BLUE = "#0000FF";
+
+let currentSolidColor = COLOR_GREEN;
+let currentColorIndex = 0;
 
 let cavnas = document.getElementById("canvas");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 var ctx = canvas.getContext("2d");
+let CIRCLE_Y = HEIGHT / 2;
 
 function draw() {
     requestAnimationFrame(draw);
+
+    drawCircle(64, CIRCLE_Y, currentSolidColor);
 }
 
 requestAnimationFrame(draw);
@@ -24,3 +32,14 @@ function drawCircle(x, y, color) {
     ctx.closePath();
     ctx.fill();
 }
+
+function getSolidColor() {
+    if (currentColorIndex == 0) {
+        currentColorIndex = 1;
+        return COLOR_BLUE;
+    } else {
+        currentColorIndex = 0;
+        return COLOR_GREEN;
+    }
+}
+
