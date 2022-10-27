49eefed4189f6ea344b7b004d72377dd28255070
Piotr Krupa <piotrkrupa06@gmail.com>
2022-10-27 16:24:17 +0200

    Add basic classes

--- lab3/script.js
+++ lab3/script.js
@@ -1,16 +1,62 @@
 const WIDTH = 800;
 const HEIGHT = 600;
 
-const RECT_WIDTH = 40;
-const RECT_SEGMENT_HEIGHT = 40;
-const FLOOR_HEIGHT = 20;
-const CIRCLE_RADIUS = 20;
-const STAIRCASE_OFFSET = 300;
-
 let cavnas = document.getElementById("canvas");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 var ctx = canvas.getContext("2d");
+
+class Player {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fill();
+    }
+}
+
+class Bullet {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    update() {
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+class Ball {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+var objects = [];
 
 function init() {
 }
@@ -19,6 +65,11 @@ function draw() {
     requestAnimationFrame(draw);
 
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    for (let object of objects) {
+        object.update();
+        object.draw();
+    }
 }
 
 init();
